2ce8cd2b60b7c6988a57ee9e06c80de6c91bc411
Yardley Gutierrez <9100062+ArkamTheBard@users.noreply.github.com>
2021-03-04 19:31:52 -0600

    Added validation to user fields and (BONUS) created annotation validation on verify password string, used th:field in add.html.

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,23 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message = "Username must be between 5-15 characters")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
+
+    @NotNull(message = "Passwords do not match")
+
+    private String verifyPassword;
 
     public User() {
 
@@ -38,5 +52,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    public void checkPassword(){
+        if(password != null && verifyPassword != null && !password.equals(verifyPassword)){
+            verifyPassword = null;
+        }
     }
 }
